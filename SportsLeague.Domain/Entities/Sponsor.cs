b8a22fc81655f4293f1e24e199009f51ce8e8865
valentinaalzate <vaev39@gmail.com>
2026-04-12 14:48:25 -0500

    edit phone y websiteurl the Domain.Entities Sponsor

--- SportsLeague.Domain/Entities/Sponsor.cs
+++ SportsLeague.Domain/Entities/Sponsor.cs
@@ -6,8 +6,8 @@ namespace SportsLeague.Domain.Entities
     {
         public string Name { get; set; } = string.Empty;
         public string ContactEmail { get; set; } = string.Empty;
-        public string? Phone { get; set; }
-        public string? WebsiteUrl { get; set; }
+        public string Phone { get; set; } = string.Empty;
+        public string WebsiteUrl { get; set; } = string.Empty;
         public SponsorCategory Category { get; set; }
 
         // Navigation Property
--- SportsLeague.Domain/Interfaces/Repositories/ISponsorRepository.cs
+++ SportsLeague.Domain/Interfaces/Repositories/ISponsorRepository.cs
@@ -1,7 +1,4 @@
 ﻿using SportsLeague.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SportsLeague.Domain.Interfaces.Repositories
 {
